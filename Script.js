4e808b6571190f632689b4463f608466656586d1
Genki <30301079+GenkiTaco@users.noreply.github.com>
2025-01-16 19:14:24 -0600

    8

--- Script.js
+++ Script.js
@@ -1,102 +1,102 @@
 //basic arithmetic operations
-let num1 = 10;
-let num2 = 5;
+    let num1 = 10;
+    let num2 = 5;
 //declaring the operations
-let total = num1 + num2;
-let difference = num1 - num2;
-let product = num1 * num2;
-let quotient = num1 / num2;
+    let total = num1 + num2;
+    let difference = num1 - num2;
+    let product = num1 * num2;
+    let quotient = num1 / num2;
 
 //calcuting the operations
-console.log(total);
-console.log(difference);
-console.log(product);
-console.log(quotient);
+//console.log(total);
+//console.log(difference);
+//console.log(product);
+//console.log(quotient);
 
 
 //Greeting with variables
-let name = "Carol";
-let age = 30;
-let city = "New York";
-let job = "Web Developer";
-let hobby = "painting";
+    let name = "Carol";
+    let age = 30;
+    let city = "New York";
+    let job = "Web Developer";
+    let hobby = "painting";
 
 //printing the greeting
 //console.log(`Hello, my name is ${name}. I am ${age} years old and I live in ${city}. I work as a ${job} and I love ${hobby}.`);
 
 //String concatenation
-let firstName = "John";
-let lastName = "Doe";
-let fullName = firstName + " " + lastName;
+    let firstName = "John";
+    let lastName = "Doe";
+    let fullName = firstName + " " + lastName;
 //printing the full name
 //console.log(fullName);
 
 //String interpolation
-let language = "JavaScript";
-let creator = "Brendan Eich";
+    let language = "JavaScript";
+    let creator = "Brendan Eich";
 //printing the string with interpolation
 //console.log(`${language} was created by ${creator}.`);
 
 //calculating cost of a meal
-let cost = 25;
-let tax = 0.08;
-let tip = 0.15;
-let taxAmount = cost * tax;
-let tipAmount = cost * tip;
-let totalCost = cost + taxAmount + tipAmount;
+    let cost = 25;
+    let tax = 0.08;
+    let tip = 0.15;
+    let taxAmount = cost * tax;
+    let tipAmount = cost * tip;
+    let totalCost = cost + taxAmount + tipAmount;
 //printing the total cost
 //console.log(`The total cost of the meal is $${totalCost}.`);
 
 //Calculating the area of a rectangle
-let width = 10;
-let height = 20;
-let area = width * height;
+    let width = 10;
+    let height = 20;
+    let area = width * height;
 //printing the area
 //console.log(`The area of the rectangle is ${area}.`);
 
 //Calculating the area of a circle
-let radius = 10;
-let pi = 3.14;
-let circleArea = pi * radius * radius;
+    let radius = 10;
+    let pi = 3.14;
+    let circleArea = pi * radius * radius;
 //printing the area
 //console.log(`The area of the circle is ${circleArea}.`);
 
 //Calculating the volume of a cylinder
-let cylinderHeight = 20;
-let cylinderVolume = pi * radius * radius * cylinderHeight;
+    let cylinderHeight = 20;
+    let cylinderVolume = pi * radius * radius * cylinderHeight;
 //printing the volume
 //console.log(`The volume of the cylinder is ${cylinderVolume}.`);
 
 //Calculating the area of a triangle
-let base = 10;
-let triangleHeight = 5;
-let triangleArea = 0.5 * base * triangleHeight;
+    let base = 10;
+    let triangleHeight = 5;
+    let triangleArea = 0.5 * base * triangleHeight;
 //printing the area
 //console.log(`The area of the triangle is ${triangleArea}.`);
 
 //Calculating the perimeter of a square
-let side = 10;
-let perimeter = 4 * side;
+    let side = 10;
+    let perimeter = 4 * side;
 //printing the perimeter
 //console.log(`The perimeter of the square is ${perimeter}.`);
 
 //Calculating the perimeter of a rectangle
-let rectangleWidth = 10;
-let rectangleHeight = 20;
-let rectanglePerimeter = 2 * (rectangleWidth + rectangleHeight);
+    let rectangleWidth = 10;
+    let rectangleHeight = 20;
+    let rectanglePerimeter = 2 * (rectangleWidth + rectangleHeight);
 //printing the perimeter
 //console.log(`The perimeter of the rectangle is ${rectanglePerimeter}.`);
 
 //Calculating the perimeter of a circle
-let circlePerimeter = 2 * pi * radius;
+    let circlePerimeter = 2 * pi * radius;
 //printing the perimeter
 //console.log(`The perimeter of the circle is ${circlePerimeter}.`);
 
 //Calculating the perimeter of a triangle
-let side1 = 10;
-let side2 = 15;
-let side3 = 20;
-let trianglePerimeter = side1 + side2 + side3;
+    let side1 = 10;
+    let side2 = 15;
+    let side3 = 20;
+    let trianglePerimeter = side1 + side2 + side3;
 //printing the perimeter
 //console.log(`The perimeter of the triangle is ${trianglePerimeter}.`);
 
